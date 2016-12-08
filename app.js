78907716ed5c312574ed34ef4fb17610ec0e203f
mdlmdel <mdlmdel@gmail.com>
2016-12-07 19:28:08 -0500

    Connect app to NYTimes Most Popular API

--- app.js
+++ app.js
@@ -2,37 +2,25 @@ $(document).ready(function () {
   // Submit event handler
   $('#search-form').submit( function(e) {
     e.preventDefault();
-    let query = $('#search-term').val();
-    console.log(query);
-    getVideos(query);
+    getMostPopular();
   })
 
-  // Get videos from YouTube
-  function getVideos(query) {
-    // Usually when in all caps, it's a constant
-    const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
+  // Get lists of most popular articles from the New York Times Magazine section 
+  function getMostPopular() {
+    var url = 'https://api.nytimes.com/svc/mostpopular/v2/mostemailed/Magazine/30.json';
     let params = {
-      q: query,
-      part: 'snippet',
-      key: 'AIzaSyDU0ALRZDh-RxFXjCOnp7SKAtlySFwFH_k'
+      q: 'query',
+      'api-key': '85b2939e3df349dd8502775e8623d350'
     }
-    $.getJSON(YOUTUBE_URL, params, function(data) {
-      // data.items is the "outbound"
-      showResults(data.items);
-      console.log(data);
-    })
+    url += '?' + $.param(params)
+    $.ajax({
+      url: url, 
+      method: 'GET',
+    }).done(function(result) {
+      console.log(result);
+    }).fail(function(err) {
+      throw err;
+    });
   }
-
-  // Display video results
-  // Video is the "inbound"
-  function showResults(videos) {
-    videos.map(function(item) {
-      console.log(item);
-      var id = item.id.videoId;
-      var title = item.snippet.title;
-      var thumbnail = item.snippet.thumbnails.medium.url;
-      $('#results').append("<li><h3 class='video-title'>" + title + 
-        "</h3><a href='http://www.youtube.com/watch?v=" + id + "'><img src='" + thumbnail + "'></a></li>");
-    })
-  }
+  
 })
